cd89083cc17ec050a57dd81f34358ab631333fe4
jennifer-kava <jenniferkava68@gmail.com>
2025-12-09 22:20:44 +0000

    Deploying to gh-pages from @ jennifer-kava/jennifer-kava.github.io@29230a4e3eff2b749f1a5e3bd8cb0eebcf823856 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-publications",
-          title: "Publications",
+          title: "publications",
           description: "A list of my publications.",
           section: "Navigation",
           handler: () => {
